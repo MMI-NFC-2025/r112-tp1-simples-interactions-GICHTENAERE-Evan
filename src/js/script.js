e0a95683db9e43076cf05c0cfe3f23a2f6f3063b
GICHTENAERE-Evan <evan.gichtenaere@edu.univ-fcomte.fr>
2025-11-07 09:57:47 +0100

    essaie du h2 final

--- src/js/script.js
+++ src/js/script.js
@@ -6,9 +6,16 @@ aMettreEnRouge.style.color='red';
 
 //rouge après clic
 const enRougeSuiteAClic = document.querySelector("#en-rouge-suite-a-click");
-enRougeSuiteAClic.addEventListener("click", (Event))=>{
+enRougeSuiteAClic.addEventListener("click", (Event)=>{
     enRougeSuiteAClic.style.color = "red"
-})
+});
+
+// const document.querySelectorAll("h2");
+// baliseH2.array.forEach(element => {
+//     element.addEventListener("click", (Event){
+//         Event.target.style.color = "red"
+//     })
+// });
 
 
 
