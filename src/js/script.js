0931178eb60d800c63f91ab984536d481253eb6d
GICHTENAERE-Evan <evan.gichtenaere@edu.univ-fcomte.fr>
2025-11-07 09:37:30 +0100

    clic rouge test

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,12 @@
 // TODO
+
+//en rouge 
 const aMettreEnRouge = document.querySelector('h2');
 aMettreEnRouge.style.color='red';
+
+//rouge après clic
+const enRougeSuiteAClic = document.querySelector("#en-rouge-suite-a-click");
+enRougeSuiteAClic.addEventListener("click", (Event))=>{
+    enRougeSuiteAClic.style.color = "red"
+})
 
